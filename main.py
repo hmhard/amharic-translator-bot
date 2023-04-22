ea65767e3ff8e8e7f38df7d4aefcbba647236962
Miniyahil Kebede <53700166+hmhard@users.noreply.github.com>
2023-04-22 18:51:49 +0300

    initial setup

---
+++ main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from textblob import TextBlob
+
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler,MessageHandler,Filters
+import requests
+import re
+import storeusersto_db as st
+# import pycountry
+
+def translate(last_message):
+    try:
+        blob=TextBlob(str(last_message))
+        tranlated_text=str(blob.translate(to='am'))
+        print(tranlated_text)
+         
+    except Exception as e:
+        print("unable to translate", e)
+        return "Unable to detect "
+    else:
+        return tranlated_text
+
+def sender(bot, update):
+    try:
+        response=""
+        # url = get_url()
+        last_message=str(update.message.text)
+        print(last_message)
+        
+        # iso_code=blob.detect_language()
+        # language = pycountry.languages.get(alpha_2=iso_code).name
+
+        retruned_text=translate(last_message)
+        response=retruned_text
+        # if retruned_text:
+        #     response="unable to detect"
+        # else:
+        #     response=retruned_text
+    #      return response
+        chat_id = update.message.chat_id
+        username=update.message.from_user.username
+        date=update.message.date
+        st.store(chat_id,username,last_message,date)
+        bot.send_message(chat_id=chat_id, text=response)
+        print(response)
+    except Exception as e:
+        print(e)
+def main():
+    updater = Updater('TOKEN')
+    dp = updater.dispatcher
+    echo_handler = MessageHandler(Filters.text, sender)
+    dp.add_handler(echo_handler)
+    # dp.add_handler(MessageHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+
+
+
+if __name__ == '__main__':
+    main()
